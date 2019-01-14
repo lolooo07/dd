0ffe4e5439f9ac2acee138615e68c47e9b0c360d
lolooo07 <lolooo07@users.noreply.github.com>
2019-01-14 00:00:00 +0000

    refactor: split code into smaller functions

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -112,4 +112,5 @@ const audioUpload = document.getElementById('audio-upload');
 const playPauseBtn = document.getElementById('play-pause-btn');
 const audioSource = document.getElementById('audio-source');
 const visualizerCanvas = document.getElementById('visualizer');
-const canvasCtx = visualizerCanvas.getContext('2d');
+const canvasCtx = visualizerCanvas.getContext('2d');
+
