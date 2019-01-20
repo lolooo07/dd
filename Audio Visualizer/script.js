14e9d602dd982f03b8ccacde3da9acaf01619f42
lolooo07 <lolooo07@users.noreply.github.com>
2019-01-20 00:00:00 +0000

    update dependencies and config files

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -127,4 +127,5 @@ function setupAudioContext() {
         analyser = audioCtx.createAnalyser();
         source = audioCtx.createMediaElementSource(audioSource);
         
-        // Connect the nodes in the audio graph
+        // Connect the nodes in the audio graph
+        source.connect(analyser);
