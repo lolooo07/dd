f866b6d464623da692479db9a80ce0f01c71824f
lolooo07 <lolooo07@users.noreply.github.com>
2019-01-22 00:00:00 +0000

    optimize code for performance

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -129,4 +129,5 @@ function setupAudioContext() {
         
         // Connect the nodes in the audio graph
         source.connect(analyser);
-        analyser.connect(audioCtx.destination);
+        analyser.connect(audioCtx.destination);
+        
