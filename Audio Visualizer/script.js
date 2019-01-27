b0b6506c2420e2cd7e9415393b2375687217ba38
lolooo07 <lolooo07@users.noreply.github.com>
2019-01-27 00:00:00 +0000

    refactor: split code into smaller functions

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -147,4 +147,5 @@ audioUpload.addEventListener('change', (e) => {
     if (file) {
         const fileURL = URL.createObjectURL(file);
         audioSource.src = fileURL;
-        playPauseBtn.disabled = false;
+        playPauseBtn.disabled = false;
+        playPauseBtn.textContent = "Play";
