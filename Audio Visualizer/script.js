b70d860f1f619f2fe088b7c6e1f99d3cb0ff5665
lolooo07 <lolooo07@users.noreply.github.com>
2019-01-28 00:00:00 +0000

    add small feature improvements

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -151,4 +151,5 @@ audioUpload.addEventListener('change', (e) => {
         playPauseBtn.textContent = "Play";
         isPlaying = false;
         
-        // Set up the audio context once a file is loaded
+        // Set up the audio context once a file is loaded
+        setupAudioContext();
