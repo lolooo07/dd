1e1e8472515d6346e77444bb06e105079ce9a4bd
lolooo07 <lolooo07@users.noreply.github.com>
2019-01-30 00:00:00 +0000

    enhance code readability

--- Audio Visualizer/script.js
+++ Audio Visualizer/script.js
@@ -159,4 +159,5 @@ audioUpload.addEventListener('change', (e) => {
 // Function to play or pause the audio
 playPauseBtn.addEventListener('click', () => {
     if (audioSource.paused) {
-        audioSource.play();
+        audioSource.play();
+        playPauseBtn.textContent = "Pause";
